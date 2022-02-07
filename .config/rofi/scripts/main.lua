010a4d0052142822d4a7a84700383c1ba7d9a316
David Burian <david.burian@me.com>
2022-02-07 10:58:39 +0100

    Rofi, zshrc npm

--- .config/rofi/scripts/main.lua
+++ .config/rofi/scripts/main.lua
@@ -213,6 +213,19 @@ function funcs.gimp(selection)
     }
   }}
 end
+function funcs.mailspring(selection)
+  if selection then
+    lib.execute_cmd('mailspring', true)
+    return nil
+  end
+
+  return {{
+    text = 'mailspring',
+    options = {
+      info = 'mailspring',
+    }
+  }}
+end
 
 function funcs.power(selection)
   if selection == 'shutdown' then
@@ -272,6 +285,7 @@ local initial_func = function()
     funcs.postman,
     funcs.slack,
     funcs.gimp,
+    funcs.mailspring,
     funcs.android_emulator,
     funcs.power,
     funcs.spotify,
