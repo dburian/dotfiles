8fd0fcc6bf6028d61ff4dc534412b0e17f804fa1
David Burian <david.burian@me.com>
2022-10-04 13:55:30 +0200

    nvim: diffrent mapping for choice nodes in luasnip

--- .config/nvim/lua/db/luasnip/init.lua
+++ .config/nvim/lua/db/luasnip/init.lua
@@ -21,23 +21,23 @@ end
 
 local map = vim.keymap.set
 
-map({'i', 's'}, '<c-j>', function ()
+map({ 'i', 's' }, '<c-j>', function()
   if ls.expand_or_jumpable() then
     ls.expand_or_jump()
   end
-end, {silent = true})
+end, { silent = true })
 
-map('n', '<c-k>', function ()
+map('n', '<c-k>', function()
   if ls.jumpable(-1) then
     ls.jump(-1)
   end
-end, {silent = true})
+end, { silent = true })
 
-map("i", "<c-h>", function()
+map("i", "<c-l>", function()
   if ls.choice_active() then
     ls.change_choice(1)
   end
-end, {silent = true})
+end, { silent = true })
 
 
 -- vim.cmd [[
