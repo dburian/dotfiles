4b664104368b64e5968a1b069d5275d9d0b6783f
David Burian <david.burian@me.com>
2022-10-05 15:03:01 +0200

    luasnip: .tmux send-keys snippet

--- .config/nvim/lua/db/luasnip/ft/dot_tmux.lua
+++ .config/nvim/lua/db/luasnip/ft/dot_tmux.lua
@@ -13,36 +13,79 @@ local ins = ls.indent_snippet_node
 local fmt = require 'luasnip.extras.fmt'.fmt
 
 local new_sess_patts = {
-  'tmux new%-session [-%w ]*%-s ?"([^"]+)"',
-  'tmux new%-session [-%w ]*%-s ?([^ ]+)',
+  'tmux new%-session [-_%w ]*%-s ?"([^"]+)"',
+  'tmux new%-session [-_%w ]*%-s ?([^ "]+)',
 }
+
+local window_patts = {
+  'tmux [-_%w "]*%-n ?"([^"]+)"',
+  'tmux [-_%w "]*%-n ?([^ "]+)',
+}
+
+local function match_patts(str, patts, return_all)
+  local matches = {}
+  for _, patt in ipairs(patts) do
+    local match = string.match(str, patt)
+    if match ~= nil then
+      table.insert(matches, match)
+      if not return_all then
+        return matches[1]
+      end
+    end
+  end
+
+  if return_all then
+    return matches
+  else
+    return nil
+  end
+end
 
 local function default_or_new_session_name()
   local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
   local default_sess_name = nil
   for _, line in ipairs(lines) do
-    for _, patt in ipairs(new_sess_patts) do
-      default_sess_name = string.match(line, patt)
-      if default_sess_name ~= nil then
-        goto double_break
-      end
+    default_sess_name = match_patts(line, new_sess_patts)
+    if default_sess_name ~= nil then
+      break
     end
   end
-  ::double_break::
-  P(lines)
-  P(default_sess_name)
-  local input_session = i(nil, 'session_name')
+  local new_session_node = i(1, 'session_name')
 
   if default_sess_name then
     return sn(nil, {
       c(1, {
         t(default_sess_name),
-        input_session,
+        new_session_node,
       })
     })
   else
-    return sn(nil, { input_session })
+    return sn(nil, { new_session_node })
   end
+end
+
+local function created_or_new_window()
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  local window_names = {}
+  for _, line in ipairs(lines) do
+    table.insert(window_names, match_patts(line, window_patts, true))
+  end
+  P(window_names)
+  window_names = vim.tbl_flatten(window_names)
+
+  local choice_nodes = vim.tbl_map(function(win_name)
+    return t(win_name)
+  end, window_names)
+  P(window_names)
+  table.insert(choice_nodes, i(1, 'window_name'))
+
+  if #choice_nodes == 1 then
+    return sn(nil, choice_nodes)
+  end
+
+  return sn(nil, {
+    c(1, choice_nodes)
+  })
 end
 
 local function tmpfile_creation(args)
@@ -51,6 +94,7 @@ local function tmpfile_creation(args)
   return sn(nil, {
     i(1),
     t({
+      '# tmpfile init',
       'tmux new-window -n "' .. sess_name .. '" -n tmpfile',
       'tmux send-keys -t "' .. sess_name .. '":tmpfile "v tmpfile.md Enter"',
       '',
@@ -79,9 +123,9 @@ local snippets = {
       t('tmux new-session -d -s "'), f(utils.same, { 1 }),
       t('" -n "'), i(2, 'default_window_name'),
       t('" -x $(tput cols) -y $(tput lines)'),
-      t({ '', '', '', }),
-      i(3, '# initialize tmux windows'),
-      t({ '', '' }),
+      t({ '', '', '' }),
+      i(3, '# window setup'),
+      t({ '', '', '' }),
       c(4, {
         t({ '# No tmpfile for this project.', '' }),
         d(nil, tmpfile_creation, { 1 })
@@ -95,6 +139,13 @@ local snippets = {
     fmt('tmux new-window -t "{}" -n "{}"', {
       d(1, default_or_new_session_name, {}),
       i(2, 'window_name')
+    })
+  ),
+  s({ trig = 'send-keys', dscr = 'Send keys to tmux window' },
+    fmt('tmux send-keys -t "{}":"{}" "{}"', {
+      d(1, default_or_new_session_name, {}),
+      d(2, created_or_new_window, {}),
+      i(3, 'keys to send')
     })
   )
 }
