f8d2ce6735b423fd39d93c1c7a681add8bd1903b
David Burian <david.burian@me.com>
2023-06-22 19:33:23 +0200

    Fix for nvim config to work without theme.vim

--- .config/nvim/plugin/options.lua
+++ .config/nvim/plugin/options.lua
@@ -8,41 +8,41 @@ opt.wildmode = 'list:longest' --on Tab complete to longest matching substring an
 
 --- Search
 opt.ignorecase = true --case-insensitive search
-opt.smartcase = true --but case-sensitive if expression contains a capital letter
+opt.smartcase = true  --but case-sensitive if expression contains a capital letter
 
 --- Buffers
 opt.hidden = true --can abandoned buffers are not unloaded but hidden
 
 --- Editor
-opt.scrolloff = 3 --show 3 lines of context around cursor
-opt.list = true --show invisible characters
+opt.scrolloff = 3     --show 3 lines of context around cursor
+opt.list = true       --show invisible characters
 
-opt.expandtab = true --use spaces instead of tabs
-opt.tabstop = 2 --global tab width
-opt.shiftwidth = 2 --spaces to use when indenting
+opt.expandtab = true  --use spaces instead of tabs
+opt.tabstop = 2       --global tab width
+opt.shiftwidth = 2    --spaces to use when indenting
 opt.shiftround = true --round to multiples of shiftwidth with '>' and '<'
 
 opt.textwidth = 0
 opt.colorcolumn = '80' --paints 80th column w/ different color
 opt.formatoptions = opt.formatoptions
-    - "a" -- Auto formatting is BAD.
-    - "t" -- Don't auto format my code. I got linters for that.
-    + "c" -- In general, I like it when comments respect textwidth
-    + "q" -- Allow formatting comments w/ gq
-    - "o" -- O and o, don't continue comments
-    + "r" -- But do continue when pressing enter.
-    + "n" -- Indent past the formatlistpat, not underneath it.
-    + "j" -- Auto-remove comments if possible.
-    - "2" -- I'm not in gradeschool anymore
+    - "a"              -- Auto formatting is BAD.
+    - "t"              -- Don't auto format my code. I got linters for that.
+    + "c"              -- In general, I like it when comments respect textwidth
+    + "q"              -- Allow formatting comments w/ gq
+    - "o"              -- O and o, don't continue comments
+    + "r"              -- But do continue when pressing enter.
+    + "n"              -- Indent past the formatlistpat, not underneath it.
+    + "j"              -- Auto-remove comments if possible.
+    - "2"              -- I'm not in gradeschool anymore
 
 
-opt.number = true --display line numbers
+opt.number = true         --display line numbers
 opt.relativenumber = true --number lines relative to current line
 
-opt.mouse = 'nv' --enable mouse for normal and visual mode
-opt.timeoutlen = 500 --timeout for key sequences
+opt.mouse = 'nv'          --enable mouse for normal and visual mode
+opt.timeoutlen = 500      --timeout for key sequences
 
-opt.laststatus = 3 -- have only global status line for all splits
+opt.laststatus = 3        -- have only global status line for all splits
 opt.foldmethod = 'marker' -- only marked folds
 
 opt.listchars = {
@@ -64,7 +64,9 @@ vim.g.tex_flavor = 'latex' --setting tex flavour for LS
 -- Configure pythons for remote plugins
 --- Colors
 vim.g.base16colorspace = 256
-vim.cmd [[
-  source ~/.config/nvim/theme.vim
-  highlight WinSeparator guibg=None
-]]
+if vim.fn.filereadable("~/.config/nvim/theme.vim") ~= 0 then
+  vim.cmd [[
+    source ~/.config/nvim/theme.vim
+    highlight WinSeparator guibg=None
+  ]]
+end
