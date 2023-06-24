62b74a4b775e49aa7a800a7c0eb97781480f1d0b
David Burian <david.burian@me.com>
2023-06-24 23:25:11 +0200

    nvim: Display full path in lualine

--- .config/nvim/after/plugin/lua_line.lua
+++ .config/nvim/after/plugin/lua_line.lua
@@ -1,1 +1,51 @@
-require('lualine').setup()
+local function shorten_filename()
+    local home = vim.env.HOME
+    local filename = vim.fn.bufname()
+
+    if filename:sub(1, #home) == home then
+        filename = '~' .. filename:sub(#home + 1, -1)
+    end
+
+    return filename
+end
+
+require('lualine').setup {
+    options = {
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = { left = '|', right = '|' },
+        section_separators = {},
+        disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = true,
+        refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+        }
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { shorten_filename },
+        lualine_x = { 'encoding', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {}
+    },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {}
+}
