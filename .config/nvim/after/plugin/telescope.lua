503d3f4d3f23d2c29da56e5d68c841e072678464
David Burian <david.burian@me.com>
2023-06-24 21:07:16 +0200

    nvim: Set unique mappings for file browser, add delete buffer mapping

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -3,39 +3,66 @@
 local keymap = require 'db.keymap'
 local nmap = keymap.nmap
 
+local actions = require 'telescope.actions'
+local actions_state = require "telescope.actions.state"
 local fb_actions = require "telescope".extensions.file_browser.actions
 local utils = require 'telescope.utils'
 local builtin = require 'telescope.builtin'
 
 require('telescope').setup {
-  pickers = {
-    git_files = {
-      show_untracked = true,
-    },
-    find_files = {
-      hidden = true,
-      no_ignore = false,
-    },
-    live_grep = {
-      additional_args = { "--hidden" }
-    },
-  },
-  extensions = {
-    -- TODO: Buffers mappings
-    -- TODO: Is this the correct way how to do mappings?
-    file_browser = {
-      hidden = true,
-      mappings = {
-        ["n"] = {
-          ["<leader>n"] = fb_actions.create,
-          ["<leader>y"] = fb_actions.copy,
-          ["<leader>c"] = fb_actions.rename,
-          ["<leader>d"] = fb_actions.remove,
-          ["-"] = fb_actions.goto_parent_dir,
+    pickers = {
+        git_files = {
+            show_untracked = true,
         },
-      },
+        find_files = {
+            hidden = true,
+            no_ignore = false,
+        },
+        live_grep = {
+            additional_args = { "--hidden" }
+        },
+        buffers = {
+            mappings = {
+                n = {
+                    -- Performes 'save buffer deletion'
+                    ["<leader>d"] = function()
+                        local selection = actions_state.get_selected_entry()
+                        local prompt_bufnr = vim.api.nvim_get_current_buf()
+
+                        if vim.bo[selection.bufnr].modified then
+                            print("Buffer " .. selection.filename .. " has unsaved changes.")
+                            actions.select_default(prompt_bufnr)
+                        else
+                            actions.delete_buffer(prompt_bufnr)
+                            actions_state.get_current_picker(prompt_bufnr):set_prompt("")
+                        end
+                    end
+                },
+
+            },
+        },
     },
-  },
+    extensions = {
+        file_browser = {
+            hidden = true,
+            mappings = {
+                n = {
+                    -- Disabling default keybindings
+                    c = false,
+                    g = false,
+                    r = false,
+                    d = false,
+                    y = false,
+
+                    ["<leader>n"] = fb_actions.create,
+                    ["<leader>y"] = fb_actions.copy,
+                    ["<leader>c"] = fb_actions.rename,
+                    ["<leader>d"] = fb_actions.remove,
+                    ["-"] = fb_actions.goto_parent_dir,
+                },
+            },
+        },
+    },
 }
 
 require('telescope').load_extension('fzf')
@@ -43,63 +70,63 @@ require('telescope').load_extension('file_browser')
 require('telescope').load_extension('markdown-links')
 
 nmap({
-  '<leader>fd',
-  function()
-    builtin.git_files({
-      -- TODO: Find dotfiles automatically
-      cwd = "~/docs/dotfiles",
-      prompt_title = "Dotfiles",
-    })
-  end,
-  { noremap = true, silent = true }
+    '<leader>fd',
+    function()
+        builtin.git_files({
+            -- TODO: Find dotfiles automatically
+            cwd = "~/docs/dotfiles",
+            prompt_title = "Dotfiles",
+        })
+    end,
+    { noremap = true, silent = true }
 })
 
 nmap({
-  '<leader>ff',
-  function()
-    local cwd = vim.uv.cwd()
-    local in_worktree = utils.get_os_command_output({ 'git', 'rev-parse', '--is-inside-work-tree' }, cwd)
-    if in_worktree[1] ~= 'true' then
-      return builtin.find_files()
-    else
-      return builtin.git_files({ show_untracked = true })
-    end
-  end,
-  { noremap = true, silent = true }
+    '<leader>ff',
+    function()
+        local cwd = vim.uv.cwd()
+        local in_worktree = utils.get_os_command_output({ 'git', 'rev-parse', '--is-inside-work-tree' }, cwd)
+        if in_worktree[1] ~= 'true' then
+            return builtin.find_files()
+        else
+            return builtin.git_files({ show_untracked = true })
+        end
+    end,
+    { noremap = true, silent = true }
 })
 
 nmap({
-  '<leader>fb',
-  builtin.buffers,
-  { noremap = true, silent = true }
+    '<leader>fb',
+    builtin.buffers,
+    { noremap = true, silent = true }
 })
 
 nmap({
-  '<leader>fg',
-  builtin.live_grep,
-  { noremap = true, silent = true }
+    '<leader>fg',
+    builtin.live_grep,
+    { noremap = true, silent = true }
 })
 
 nmap({
-  '<leader>ft',
-  function()
-    require("telescope").extensions.file_browser.file_browser({
-      path = vim.fn.expand("%:p:h")
-    })
-  end,
-  { noremap = true, silent = true }
+    '<leader>ft',
+    function()
+        require("telescope").extensions.file_browser.file_browser({
+            path = vim.fn.expand("%:p:h")
+        })
+    end,
+    { noremap = true, silent = true }
 })
 nmap({
-  '<leader>fp',
-  function()
-    builtin.find_files {
-      cwd = '~/.local/share/nvim/bundle/',
-    }
-  end,
-  { noremap = true, silent = true }
+    '<leader>fp',
+    function()
+        builtin.find_files {
+            cwd = '~/.local/share/nvim/bundle/',
+        }
+    end,
+    { noremap = true, silent = true }
 })
 nmap({
-  '<leader>fh',
-  builtin.help_tags,
-  { noremap = true, silent = true }
+    '<leader>fh',
+    builtin.help_tags,
+    { noremap = true, silent = true }
 })
--- README.md
+++ README.md
@@ -22,12 +22,12 @@ In case of any questions, feel free to raise an issue.
 ### TODO
 
 - nvim
-    - null-ls and lsp low workload setting -- done but I would prefer to have
-      start, stop functions with server name as argument
-    - unique mappings for telescope file explorer
-    - mapping for buffer deletion in telescope buffers
     - perhaps https://github.com/nvim-telescope/telescope-ui-select.nvim
     - treesitter markdown custom queries and highlighting
+    - packer/lua based plugin manager instead of plug
+    - mason plugin
+    - theprimagen mappings
+    - proper lualine configuration
 - rofi
     - bluetooth script
     - flash disks -- mounting
