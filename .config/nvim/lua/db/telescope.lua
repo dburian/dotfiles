3f1a3ecb0ca56539caee4cbe6040356161e40a38
David Burian <david.burian@me.com>
2023-07-08 02:51:59 +0200

    nvim: Add tag search for given tag with ripgrep.

--- .config/nvim/lua/db/telescope.lua
+++ .config/nvim/lua/db/telescope.lua
@@ -1,7 +1,12 @@
 local pickers = require 'telescope.pickers'
+local previewers = require "telescope.previewers"
+local action_set = require "telescope.actions.set"
+local action_state = require "telescope.actions.state"
+local flatten = vim.tbl_flatten
 local finders = require 'telescope.finders'
 local conf = require 'telescope.config'.values
 local nmap = require 'db.keymap'.nmap
+local make_entry = require "telescope.make_entry"
 local entry_display = require 'telescope.pickers.entry_display'
 
 -- TODO: Wrap this into a plugin (telescope extension)
@@ -69,7 +74,6 @@ local telescope_tag_select = function(opts)
 
   local tagname = opts.tagname or vim.fn.expand("<cword>")
 
-  P(tagname)
   local matching_tags = vim.fn.taglist("^" .. tagname .. "$")
   if #matching_tags == 0 then
     vim.notify("No tags match '" .. tagname .. "'.", vim.log.levels.INFO)
@@ -94,4 +98,92 @@ local telescope_tag_select = function(opts)
   }):find()
 end
 
+local telescope_tag_select_rg = function(opts)
+  opts = opts or {}
+  opts.bufnr = 0
+
+  if vim.fn.executable("rg") == 0 then
+    vim.notify("You need to install rg.", vim.log.levels.ERROR)
+    return
+  end
+
+  local tagname = opts.tagname or (opts.search_cword and vim.fn.expand("<cword>")) or nil
+
+  local tags_command = { "rg", "-H", "-N", "--no-heading", "--color", "never" }
+  if tagname == nil then
+    -- find lines not matching tags file format (begins with !) or empty lines.
+    table.insert(tags_command, "-v")
+    table.insert(tags_command, "^!|^$")
+  else
+    -- find lines matching our tagname
+    table.insert(tags_command, "-e")
+    table.insert(tags_command, "^" .. tagname .. '\t')
+  end
+
+  local tagfiles = opts.ctags_file and { opts.ctags_file } or vim.fn.tagfiles()
+  for i, ctags_file in ipairs(tagfiles) do
+    tagfiles[i] = vim.fn.expand(ctags_file, true)
+  end
+  if vim.tbl_isempty(tagfiles) then
+    vim.notify("No tags file found. Create one with ctags -R", vim.log.levels.ERROR)
+    return
+  end
+
+  opts.entry_maker = vim.F.if_nil(opts.entry_maker, make_entry.gen_from_ctags(opts))
+
+  pickers
+      .new(opts, {
+        prompt_title = tagname and "Tags matching '" .. tagname .. "'" or "Tags",
+        finder = finders.new_oneshot_job(flatten { tags_command, tagfiles }, opts),
+        previewer = previewers.ctags.new(opts),
+        sorter = conf.generic_sorter(opts),
+        on_complete = { function(picker)
+          -- remove this on_complete callback
+          picker:clear_completion_callbacks()
+          -- if we have exactly one match, select it
+          local num_of_entries = picker.manager.linked_states.size
+          if num_of_entries == 1 then
+            require("telescope.actions").select_default(picker.prompt_bufnr)
+          elseif num_of_entries == 0 then
+            require "telescope.actions".close(picker.prompt_bufnr)
+            if tagname == nil then
+              vim.notify("No tags found.", vim.log.levels.INFO)
+            else
+              vim.notify("No tags matching '" .. tagname "' found.", vim.log.levels.INFO)
+            end
+          end
+        end,
+        },
+        attach_mappings = function()
+          action_set.select:enhance {
+            post = function()
+              local selection = action_state.get_selected_entry()
+              if not selection then
+                return
+              end
+
+              if selection.scode then
+                -- un-escape / then escape required
+                -- special chars for vim.fn.search()
+                -- ] ~ *
+                local scode = selection.scode:gsub([[\/]], "/"):gsub("[%]~*]", function(x)
+                  return "\\" .. x
+                end)
+
+                vim.cmd "keepj norm! gg"
+                vim.fn.search(scode)
+                vim.cmd "norm! zz"
+              else
+                vim.api.nvim_win_set_cursor(0, { selection.lnum, 0 })
+              end
+            end,
+          }
+          return true
+        end,
+      })
+      :find()
+end
+
 nmap({ '<leader>gd', telescope_tag_select, { noremap = true } })
+nmap({ '<leader>fv', function() telescope_tag_select_rg({ search_cword = true, fname_width = 40 }) end,
+  { noremap = true } })
