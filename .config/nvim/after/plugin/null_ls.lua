750092b2d020b44be3616d4489d1dd3176cef1bc
David Burian <david.burian@me.com>
2023-09-29 10:25:53 +0200

    null_ls: Moving from pylint to ruff

--- .config/nvim/after/plugin/null_ls.lua
+++ .config/nvim/after/plugin/null_ls.lua
@@ -22,46 +22,49 @@ local is_exec = function(prg)
 end
 
 local source_configs = {
-  { 'black',       null_ls.builtins.formatting.black },
-  { 'isort',       null_ls.builtins.formatting.isort },
+  { 'black', null_ls.builtins.formatting.black },
   { 'latexindent', null_ls.builtins.formatting.latexindent },
-  { 'eslint_d',    null_ls.builtins.diagnostics.eslint_d },
-  { 'pyling', null_ls.builtins.diagnostics.pylint.with({
-    diagnostics_postprocess = function(diagnostic)
-      diagnostic.message = '[' .. diagnostic.symbol .. ']: ' .. diagnostic.message
-    end,
-    extra_args = function()
-      -- Set by 'activating' the virtual environment
-      local venv = vim.env.VIRTUAL_ENV
-      if venv == nil then
-        return {}
-      end
+  { 'eslint_d', null_ls.builtins.diagnostics.eslint_d },
+  { 'ruff', null_ls.builtins.formatting.ruff },
+  { 'ruff', null_ls.builtins.diagnostics.ruff },
+  -- Superseded by ruff
+  -- { 'isort', null_ls.builtins.formatting.isort },
+  -- { 'pylint', null_ls.builtins.diagnostics.pylint.with({
+  --   diagnostics_postprocess = function(diagnostic)
+  --     diagnostic.message = '[' .. diagnostic.symbol .. ']: ' .. diagnostic.message
+  --   end,
+  --   extra_args = function()
+  --     -- Set by 'activating' the virtual environment
+  --     local venv = vim.env.VIRTUAL_ENV
+  --     if venv == nil then
+  --       return {}
+  --     end
 
-      local site_packages_path = vim.fn.glob(venv .. '/lib/*/site-packages/')
-      local root_path = find_project_root(vim.fn.expand('%:p'))
+  --     local site_packages_path = vim.fn.glob(venv .. '/lib/*/site-packages/')
+  --     local root_path = find_project_root(vim.fn.expand('%:p'))
 
-      local extra_sys_paths = { site_packages_path }
-      if root_path ~= nil then
-        table.insert(extra_sys_paths, root_path)
-      end
+  --     local extra_sys_paths = { site_packages_path }
+  --     if root_path ~= nil then
+  --       table.insert(extra_sys_paths, root_path)
+  --     end
 
-      local append_str = ''
-      for _, path in ipairs(extra_sys_paths) do
-        append_str = append_str .. 'sys.path.append("' .. path .. '"); '
-      end
+  --     local append_str = ''
+  --     for _, path in ipairs(extra_sys_paths) do
+  --       append_str = append_str .. 'sys.path.append("' .. path .. '"); '
+  --     end
 
-      -- Pylint runs in different process (I guess) so it does not know about
-      -- activated virtual environments as nvim does. Adding site-packages to
-      -- path overcomes this issue.
-      return {
-        '--rcfile', '~/.config/pylint/config.toml',
-        '--py-version', python_helpers.get_python_version(),
-        '--init-hook', 'import sys; ' .. append_str,
-      }
-    end
-  }) },
+  --     -- Pylint runs in different process (I guess) so it does not know about
+  --     -- activated virtual environments as nvim does. Adding site-packages to
+  --     -- path overcomes this issue.
+  --     return {
+  --       '--rcfile', '~/.config/pylint/config.toml',
+  --       '--py-version', python_helpers.get_python_version(),
+  --       '--init-hook', 'import sys; ' .. append_str,
+  --     }
+  --   end
+  -- }) },
   { 'chktex', null_ls.builtins.diagnostics.chktex },
-  { 'mypy',   null_ls.builtins.diagnostics.mypy },
+  { 'mypy', null_ls.builtins.diagnostics.mypy },
 }
 
 local sources = {}
