bf64c30967f2ce9047a3f2545af77b53082e12d5
David Burian <david.burian@me.com>
2025-09-07 10:41:18 +0200

    nvim: don't display mode

--- .config/nvim/plugin/options.lua
+++ .config/nvim/plugin/options.lua
@@ -8,7 +8,7 @@ opt.wildmode = 'list:longest' --on Tab complete to longest matching substring an
 
 --- Search
 opt.ignorecase = true --case-insensitive search
-opt.smartcase = true --but case-sensitive if expression contains a capital letter
+opt.smartcase = true  --but case-sensitive if expression contains a capital letter
 
 --- Buffers
 opt.hidden = true --can abandoned buffers are not unloaded but hidden
@@ -20,44 +20,45 @@ opt.undodir = vim.fn.stdpath('cache') .. '/nvim/undodir'
 opt.undofile = true
 
 --- Editor
-opt.scrolloff = 8 --show 8 lines of context around cursor
-opt.list = true --show invisible characters
+opt.scrolloff = 8     --show 8 lines of context around cursor
+opt.list = true       --show invisible characters
+opt.showmode = false  -- don't print '--INSERT--' in the command line when in insert mode (or other modes)
 
-opt.expandtab = true --use spaces instead of tabs
-opt.tabstop = 2 --global tab width
-opt.shiftwidth = 2 --spaces to use when indenting
+opt.expandtab = true  --use spaces instead of tabs
+opt.tabstop = 2       --global tab width
+opt.shiftwidth = 2    --spaces to use when indenting
 opt.shiftround = true --round to multiples of shiftwidth with '>' and '<'
 
 opt.textwidth = 0
 opt.colorcolumn = '80' --paints 80th column w/ different color
 opt.formatoptions = opt.formatoptions
-    - "a" -- Auto formatting is BAD.
-    - "t" -- Don't auto format my code. I got linters for that.
-    + "c" -- In general, I like it when comments respect textwidth
-    + "q" -- Allow formatting comments w/ gq
-    - "o" -- O and o, don't continue comments
-    + "r" -- But do continue when pressing enter.
-    + "n" -- Indent past the formatlistpat, not underneath it.
-    + "j" -- Auto-remove comments if possible.
-    - "2" -- I'm not in gradeschool anymore
+    - "a"              -- Auto formatting is BAD.
+    - "t"              -- Don't auto format my code. I got linters for that.
+    + "c"              -- In general, I like it when comments respect textwidth
+    + "q"              -- Allow formatting comments w/ gq
+    - "o"              -- O and o, don't continue comments
+    + "r"              -- But do continue when pressing enter.
+    + "n"              -- Indent past the formatlistpat, not underneath it.
+    + "j"              -- Auto-remove comments if possible.
+    - "2"              -- I'm not in gradeschool anymore
 
 
-opt.number = true --display line numbers
+opt.number = true         --display line numbers
 opt.relativenumber = true --number lines relative to current line
 
-opt.mouse = 'nv' --enable mouse for normal and visual mode
-opt.timeoutlen = 500 --timeout for key sequences
+opt.mouse = 'nv'          --enable mouse for normal and visual mode
+opt.timeoutlen = 500      --timeout for key sequences
 
-opt.laststatus = 3 -- have only global status line for all splits
+opt.laststatus = 3        -- have only global status line for all splits
 opt.foldmethod = 'marker' -- only marked folds
 
 opt.listchars = {
-    trail = '·',
-    nbsp = '␣',
-    tab = '» ',
-    extends = '<',
-    precedes = '>',
-    conceal = '┊',
+  trail = '·',
+  nbsp = '␣',
+  tab = '» ',
+  extends = '<',
+  precedes = '>',
+  conceal = '┊',
 }
 
 opt.tags = './tags;$HOME'
